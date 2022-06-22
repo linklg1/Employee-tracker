2bc5e8e5ae249f4e5369e1e41396eb9a50442e4c
Zelda <lumakawar@hotmail.com>
2022-06-21 21:53:10 -0400

    foreignkey

--- employees_db.sql
+++ employees_db.sql
@@ -4,14 +4,15 @@ USE employee_DB;
 
 CREATE TABLE departments (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE roles (
     id INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL, 
-    dept_id  INT NOT NULL
+    dept_id  INT NOT NULL,
+    FOREIGN KEY (dept_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
@@ -19,5 +20,7 @@ CREATE TABLE employees (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NULL
+    manager_id INT NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
